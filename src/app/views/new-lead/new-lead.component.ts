91f24a8e82b429feb65d5a70f3217a95312d87f8
Matheus Garrido <matheusgarrido10@hotmail.com>
2021-07-09 22:21:17 -0300

    Auto-generated commit

--- src/app/views/new-lead/new-lead.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-new-lead',
-  templateUrl: './new-lead.component.html',
-  styleUrls: ['./new-lead.component.scss'],
-})
-export class NewLeadComponent {}
