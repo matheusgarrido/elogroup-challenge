50e232680968c48eae930d1331bf472197cf98e9
Matheus Garrido <matheusgarrido10@hotmail.com>
2021-07-11 15:45:54 -0300

    Expand/collapse lead info in table

--- src/app/components/card-lead/card-lead.component.ts
+++ src/app/components/card-lead/card-lead.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { leadData } from 'src/app/services/lead/lead.service';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-lead',
@@ -15,5 +14,8 @@ export class CardLeadComponent {
     tableColumn: 0,
   };
 
-  constructor() {}
+  infoActive = false;
+  changeInfoActive() {
+    this.infoActive = !this.infoActive;
+  }
 }
